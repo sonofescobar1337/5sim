9e7772c4d3283b6375cbb2d16f897f6e7c7c062d
sonofescobar1337 <argrdotid@gmail.com>
2024-06-09 19:19:53 +0700

    Under dev bang

---
+++ src/Purchase/BuyActivationNumberVoiceForwarding.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch';
+
+
+
+export default async function BuyActivationNumberVoiceForwarding(api, country, operator, product, number, reuse = 0, voice = 0,) {
+    try {
+        // BELOM KELAR BOS
+    } catch (error) {
+        return { error }
+    }
+}
