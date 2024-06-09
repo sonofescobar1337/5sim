d76d943bf92b462b4883a3512b20896e59a9d677
sonofescobar1337 <argrdotid@gmail.com>
2024-06-09 17:42:37 +0700

    add limit on history order (custom limit)

--- src/accounts/HistoryOrder.js
+++ src/accounts/HistoryOrder.js
@@ -3,11 +3,11 @@ import fetch from 'node-fetch';
 
 
 
-export default async function historyorder(apikey) {
+export default async function historyorder(apikey, limit = 15) {
 
 
     try {
-        const url = await fetch('https://5sim.net/v1/user/orders?category=activation&limit=15&offset=0&order=id&reverse=true', {
+        const url = await fetch(`https://5sim.net/v1/user/orders?category=activation&limit=${limit}&offset=0&order=id&reverse=true`, {
             headers: {
                 'Authorization': `Bearer ${apikey}` ,
                 'Accept': 'application/json'
