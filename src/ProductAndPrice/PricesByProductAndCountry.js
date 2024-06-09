2c37ea5bcfa7fa2d41a215d7397be63ccf43fbb4
sonofescobar1337 <argrdotid@gmail.com>
2024-06-09 18:41:20 +0700

    add prices by product and country module

---
+++ src/ProductAndPrice/PricesByProductAndCountry.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+
+
+
+export default async function PricesByProductAndCountry(country, product) {
+
+    try {
+        const url = await fetch(`https://5sim.net/v1/guest/prices?country=${country}&product=${product}`, {
+            headers: {
+              'Accept': 'application/json'
+            }
+          });
+
+          const response = await url.json();
+
+          return { response }
+    } catch (error) {
+        return { error }
+    }
+}
